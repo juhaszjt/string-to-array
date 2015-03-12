b066f1f301f8e0a21b349940ca839e1b5661cc0e
juhaszjt <juhaszjt@gmail.com>
2015-03-12 16:34:19 +0100

    settings test

---
+++ test/StringToArrayTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tdd\Test\StringToArray;
+
+use Tdd\StringToArray\StringToArray;
+
+class StringToArrayTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * Test settings
+	 */
+	public function testSettings()
+	{
+		$this->assertFalse(true);
+	}
+}
+ 
